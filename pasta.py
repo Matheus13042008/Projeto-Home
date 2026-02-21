1e27736ebe05a568570322522d1ac208061e51fd
Matheus13042008 <mathbraga.1304@gmail.com>
2026-02-21 20:06:41 -0300

    bora

--- pasta.py
+++ pasta.py
@@ -1,10 +1,2 @@
-print('Formulário')
-nome = input('Digite seu Nome:\n')
-sobrenome = input('Digite seu sobrenome:\n')
-print('Confirma Que Seu Nome Então É:', (nome.upper()), (sobrenome.upper()))
-nascimento = input('Digite quando você nasceu, incluindo dia, mês e ano:\n')
-universidade = input('Digite qual universidade está cursando:\n')
-
-email = (nome.lower()) + '.' + (sobrenome.lower()) + '@' + (universidade.lower()) +'.br'
-print('Seu email é:',email)
-print('Sua senha é:''a' + str(email.count('a')) + 'e' + str(email.count('e')) + 'i' + str(email.count('i')) + 'o' + str(email.count('o')) + 'u' + str(email.count('u')))
+nome = input('qual seu nome')
+print('CONSEGUI')
